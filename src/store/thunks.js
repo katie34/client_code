f4b5fed8997ab4c6fd86b43b6413d21aae0e530c
katie34 <89612931+katie34@users.noreply.github.com>
2021-12-19 13:35:23 -0500

    updating const allCampuses

--- src/store/thunks.js
+++ src/store/thunks.js
@@ -78,6 +78,7 @@ export const fetchStudentThunk = id => async dispatch => {
     console.error(err);
   }
 };
+
 //Add campus
 export const addCampusThunk = (campus) => async (dispatch) => {
   try {
