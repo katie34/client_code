3bbcaeca121e537a8250e82223d26232241807df
katie34 <89612931+katie34@users.noreply.github.com>
2021-12-19 13:56:16 -0500

    updated campus view file, copied homepage view code and edit to fit function

--- src/components/views/CampusView.js
+++ src/components/views/CampusView.js
@@ -1,22 +1,86 @@
+import { Link } from "react-router-dom";
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import { makeStyles } from '@material-ui/core/styles';
 
+const useStyles = makeStyles(theme => ({
+  root: {
+    flexGrow: 1,
+  },
+  title: {
+    flexGrow: 1,
+    textAlign: 'left',
+    fontType: 'bold',
+    fontFamily: 'Courier, sans-serif', 
+    fontSize: '35px', 
+    color: '#CDDC39'
+  },
+  appBar:{
+    backgroundColor: '#11153e',
+    shadows: ['none'],
+  },
+  greeting:{
+    display: 'flex',
+    justifyContent: 'center',
+    backgroundColor: 'white',
+    width: "50%",
+    margin: "auto",
+  },
+  links:{
+    textDecoration: 'none',
+  }
+
+}));
 
 const CampusView = (props) => {
-    const {campus} = props;
-    return (
-      <div>      
-        <h1>{campus.name}</h1>
-        <p>{campus.description}</p>
-        <ul>
-        {campus.students.map( student => {
-          let name = student.firstname + " " + student.lastname;
-          return (
-            <li key={student.id}>{name}</li>
-          );
-        })}
-        </ul>
-      </div>
-    );
-  
-  };
-  
-  export default CampusView;
+  const classes = useStyles();
+  const {campus} = props;
+  return (
+    <div>
+      <div className={classes.root}>
+      <AppBar position="static" elevation={0} className={classes.appBar}>
+        <Toolbar>
+          <Typography variant="h6" className={classes.title} color="inherit" >
+            University Directory
+          </Typography>
+
+          <Link className={classes.links} to={'/'} >
+            <Button variant="contained" color="primary" style={{marginRight: '10px'}}>
+              Home
+            </Button>
+          </Link>
+
+          <Link className={classes.links} to={'/campuses'} >
+            <Button variant="contained" color="primary" style={{marginRight: '10px'}}>
+              All Campuses
+            </Button>
+          </Link>
+
+          <Link className={classes.links} to={'/students'} >
+            <Button variant="contained" color="primary">
+              All Students
+            </Button>
+          </Link>
+        </Toolbar>
+      </AppBar>
+    </div>      
+      <h1>Name: {campus.name}</h1>
+      <h2>Description: {campus.description}</h2>
+      <img src={campus.imgurl} height = '150' width = '250'/>
+      <h3>Address: {campus.address}</h3>
+      <ul>
+      {campus.students.map( student => {
+        let name = student.firstname + " " + student.lastname;
+        return (
+          <li key={student.id}>{name}</li>
+        );
+      })}
+      </ul>
+    </div>
+  );
+
+};
+
+export default CampusView;
